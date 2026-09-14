4beaade4ca28bceecfc834eb1d3b201237a9e01d
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-14 16:44:12 +0530

    Add files via upload

---
+++ helloganeshji.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+    cout << "happy ganesh chaturthi !";
+    return 0;
+}
