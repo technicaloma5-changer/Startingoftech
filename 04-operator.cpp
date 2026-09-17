7f70a1754ef89caf7fd6702f455f70984ab5794f
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-17 22:34:14 +0530

    Add files via upload

---
+++ 04-operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Taking two numbers
+    int a = 10;
+    int b = 3;
+
+    // Performing basic arithmetic operations
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Remainder: " << a % b << endl;
+
+    // Checking the values using comparison operators
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+
+    return 0;
+}
