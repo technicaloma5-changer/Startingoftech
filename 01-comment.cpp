afdb8577b9786ff1cbb94cfe4154b5791145d0e9
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-20 22:14:19 +0530

    Add files via upload

---
+++ 01-comment.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is a single-line comment
+    cout << "Hello, World!" << endl;
+
+    /*
+        This is a
+        multi-line comment.
+        It can span multiple lines.
+    */
+
+    // Printing a message
+    cout << "Learning C++ comments!" << endl;
+
+    return 0;
+}
