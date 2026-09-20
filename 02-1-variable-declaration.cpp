ccf7c6b9a5a3711b11f33c631c8c3affa6cfd109
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-20 22:05:45 +0530

    Add files via upload

---
+++ 02-1-variable-declaration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Variable declaration
+
+    // Syntax:
+    // data_type variable_name;
+
+    int age;        //stores integer value
+    float height;   //stores float value
+    double salary;  //stores double value
+    char grade;     //stores character value
+    bool isStudent; //stores Boolean  value
+
+    return 0;
+}
