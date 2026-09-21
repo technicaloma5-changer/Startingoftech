298b29b4f412ea91f3c567438bca426ec7869ee6
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-21 23:12:16 +0530

    Add files via upload

---
+++ 02-2-datatype-float-double.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // float and double are used to store decimal numbers.
+    // float generally provides less precision than double.
+    // double provides more precision and is commonly used
+    // when more accurate decimal values are needed.
+
+    float height = 5.8f;
+    float temperature = 36.5f;
+
+    double pi = 3.14159265359;
+    double distance = 12345.6789;
+
+    // Displaying float values
+    cout << "Height: " << height << endl;
+    cout << "Temperature: " << temperature << endl;
+
+    // Displaying double values
+    cout << "Value of Pi: " << pi << endl;
+    cout << "Distance: " << distance << endl;
+
+    return 0;
+}
