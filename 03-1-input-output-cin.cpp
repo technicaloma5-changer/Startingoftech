73bdf3b09a68ecf924579f00f18af8a35245c206
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-24 23:17:55 +0530

    Add files via upload

---
+++ 03-1-input-output-cin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable to store the age entered by the user
+    int age;
+
+    // Ask the user to enter their age
+    cout << "Enter your age: ";
+
+    // Take age as input from the user
+    cin >> age;
+
+    // Display the entered age
+    cout << "Your age is: " << age << endl;
+
+    return 0;
+}
