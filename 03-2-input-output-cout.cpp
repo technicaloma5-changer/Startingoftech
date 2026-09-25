a3926f693696015d3e7647db981a408e9cc53d6c
technicaloma5-changer <ghugeom058@gmail.com>
2026-09-25 21:51:50 +0530

    Add files via upload

---
+++ 03-2-input-output-cout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Printing a simple message
+    cout << "Hello, C++!" << endl;
+
+    // Printing numbers
+    cout << 100 << endl;
+
+    // Printing multiple values
+    cout << "Age: " << 18 << endl;
+
+    // Printing a calculation
+    cout << "Sum: " << 10 + 20 << endl;
+
+    return 0;
+}
